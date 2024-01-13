c5749a92c789dbdb9e3237ffa20e215c933fb464
Amit Jha MacBook Pro <atjhamit@gmail.com>
2024-01-13 11:01:31 +0530

    Adding few comments for pass by reference

--- basics/tricky/03_passByReference.cpp
+++ basics/tricky/03_passByReference.cpp
@@ -6,6 +6,12 @@ void changeValue(int p, int &q)
 {
     p = 5;
     q = 6;
+
+    int k = 9;
+    // here q is taking the value of k, q is still the nickname of b only, nickname is only set once!
+    // that is why you must do declaration and initialization of reference in a single line itself and it cannot be changed later
+    q = k; 
+    
 }
 
 int main()
