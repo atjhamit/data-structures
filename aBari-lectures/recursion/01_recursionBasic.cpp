f1d5009ca26176f047436a318752cefb2b92a9d0
Amit Jha MacBook Pro <atjhamit@gmail.com>
2024-01-14 12:59:48 +0530

    recursion basics

---
+++ aBari-lectures/recursion/01_recursionBasic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define LOG(x)      std::cout << x << "\t"
+#define LOG_L(x)    std::cout << x << "\n"
+
+void func(int n)
+{
+    if(n > 0)
+    {
+        LOG(n);
+        func(n - 1);
+    }
+}
+
+int main()
+{
+    int x = 3;
+    func(x);
+    LOG_L("");
+
+    return 0;
+}
---
+++ aBari-lectures/recursion/templateRecusion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define LOG(x)      std::cout << x << "\t"
+#define LOG_L(x)    std::cout << x << "\n"
+
+void func()
+{
+    ;
+}
+
+int main()
+{
+    ;
+
+    return 0;
+}
