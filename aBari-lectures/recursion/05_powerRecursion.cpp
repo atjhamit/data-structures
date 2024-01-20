14d40106224f53b3d3f828ef60332009085c84d4
Amit Jha MacBook Pro <atjhamit@gmail.com>
2024-01-20 08:28:15 +0530

    Commit pending from 19th jan : indirectRecursion

---
+++ aBari-lectures/recursion/05_powerRecursion.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#define LOG(x)      std::cout << x << "\t"
+#define LOG_L(x)    std::cout << x << "\n"
+
+int powSlow(int m, int n)
+{
+    if(n <= 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return m * powSlow(m, n - 1);
+    }
+}
+
+int powFast(int m, int n)
+{
+    if(n <= 0)
+    {
+        return 1;
+    }
+    
+    if(n % 2 == 0)
+    {
+        return powFast(m * m, n / 2);
+    }
+    else
+    {
+        return m * powFast(m * m, (n - 1) / 2);
+    }
+}
+
+int main()
+{
+    int m = 2;
+    int n = 9;
+
+    LOG_L("Ans : " << powSlow(m, n));
+    LOG_L("Ans : " << powFast(m, n));
+
+    return 0;
+}
