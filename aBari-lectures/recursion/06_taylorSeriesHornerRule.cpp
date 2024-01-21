eaf410b95341c6dfa17e49d9bb25d1c81719e165
Amit Jha MacBook Pro <atjhamit@gmail.com>
2024-01-21 12:52:23 +0530

    Commit of 20th Jan : taylor series

---
+++ aBari-lectures/recursion/06_taylorSeriesHornerRule.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+#define LOG(x)      std::cout << x << "\t"
+#define LOG_L(x)    std::cout << x << "\n"
+
+double facto(double n)
+{
+    
+    if( n < 1)
+    {
+        return 1;
+    }
+    return n * facto(n - 1);
+}
+
+double pow(double m, double n)
+{
+    if(n < 1)
+    {
+        return 1;
+    }
+    
+    if((int)n % 2 == 0)
+    {
+        return pow(m * m, n / 2);
+    }
+
+    return m * pow(m * m, (n - 1) / 2);
+}
+
+double taylorSeriesSelf(double x, double n)
+{
+    if(n < 1)
+    {
+        return 1;
+    }
+
+    return taylorSeriesSelf(x, n - 1) + (pow(x , n) / facto(n));
+}
+
+double taylorSeriesOptimized(double x, double n)
+{
+    static double num = 1;
+    static double den = 1;
+
+    if(n < 1)
+    {
+        return 1;
+    }
+
+    double result = taylorSeriesOptimized(x, n - 1);
+    num *= x;
+    den *= n;
+    return (double)result + num / den;
+}
+
+double hornerLoop(double x, double n)
+{
+    double res = 1;
+
+    for (int i = n; i > 0; i--)
+    {
+        res = 1 + (res * (x / i));
+    }
+    return res;
+}
+
+double hornerRecursive(double x, double n)
+{
+    static double res = 1;
+    if(n < 1)
+    {
+        return res;
+    }
+
+    res = 1 + ((x / n) * res);
+
+    return hornerRecursive(x , n - 1);
+}
+
+int main()
+{
+    double x = 4;
+    double n = 15;
+
+    LOG_L("Taylor series Self: \t" << taylorSeriesSelf(x, n));
+    LOG_L("Taylor series Optimized: \t" << taylorSeriesOptimized(x, n));
+    LOG_L("Honer's rule iterative:\t " << hornerLoop(x, n));
+    LOG_L("Horner's rule recursive: \t" << hornerRecursive(x, n));
+
+    return 0;
+}
