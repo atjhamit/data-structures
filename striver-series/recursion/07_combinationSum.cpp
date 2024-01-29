662c7636a72f47f8501986b3100fe19af6ae8ad9
Amit Kumar Jha <atjhamit@gmail.com>
2024-01-29 15:23:17 +0000

    Combination Sum

--- striver-series/recursion/07_combinationSum.cpp
+++ striver-series/recursion/07_combinationSum.cpp
@@ -6,12 +6,12 @@
 
 using namespace std;
 
-class Solution 
+class Solution
 {
-    vector<vector<int>> getCombinationSum(int index, const vector<int>& candidates, 
-                                            int target, vector<int> temp)
+    vector<vector<int>> getCombinationSum(int index, const vector<int>& candidates,
+                                            int target, vector<int> temp,
+                                            vector<vector<int>>& result)
     {
-        static vector<vector<int>> result;
         if(target <= 0)
         {
             if(target == 0)
@@ -20,29 +20,30 @@ class Solution
             }
             return result;
         }
-        if(index > candidates.size())
+        if(index == candidates.size())
         {
             return result;
         }
 
         temp.push_back(candidates[index]);
-        getCombinationSum(index, candidates, target - candidates[index], temp);
+        getCombinationSum(index, candidates, target - candidates[index], temp, result);
         temp.pop_back();
-        getCombinationSum(index + 1, candidates, target, temp);
+        getCombinationSum(index + 1, candidates, target, temp, result);
         return result;
     }
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
     {
-        return getCombinationSum(0, candidates, target, {});
+        vector<vector<int>> result;
+        return getCombinationSum(0, candidates, target, {}, result);
     }
 };
 
 int main()
 {
     vector<vector<int>> result;
-    vector<int> candidates = {2, 3, 6, 7};
-    int target = 7;
+    vector<int> candidates = {2, 3, 5};
+    int target = 8;
 
     Solution obj;
     result = obj.combinationSum(candidates, target);
